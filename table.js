a3cfad67603bd9b3465a1c255d1b98141f20a374
jslopeza <jasonlopeza95@gmail.com>
2014-07-13 01:40:43 -0400

    Using jquery Datatables plugin

---
+++ table.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('#table').dataTable({
+		"ajax" : 'getMovieInfo.php',
+		//"datasrc" : 'movies',
+		"aoColumns" : [
+			{"mDataProp" : "poster", "mRender" : function(obj){
+				return '<img width="100px height="100px" src="'+obj+'">';
+			}},
+			{"mDataProp" : "title", 'sType' : 'string'},
+			{"mDataProp" : "year", "sType" : "number"},
+			{"mDataProp" : "rated","sType" : "number"},
+			{"mDataProp" : "runtime","sType" : "number"},
+			{"mDataProp" : "genre"},
+			{"mDataProp" : "director", "sWidth" : "150px"},
+			{"mDataProp" : "actors"},
+		],
+	});
+});
